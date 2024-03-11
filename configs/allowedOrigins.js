5230e33a4bda987e2cdefe06c33a9f99c69b35e9
Vulpes231 <whykayshot@gmail.com>
2024-03-11 12:43:40 +0100

    fix-cors

--- configs/allowedOrigins.js
+++ configs/allowedOrigins.js
@@ -3,6 +3,7 @@ const allowedOrigins = [
   "http://localhost:5173",
   "https://bishop-server.onrender.com",
   "https://main--scepter2024.netlify.app",
+  "https://psychedelicsng.online"
 ];
 
 module.exports = { allowedOrigins };
