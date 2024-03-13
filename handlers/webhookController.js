0fcf9121a651db8c7f241f0467ccb7130db6839f
Vulpes231 <whykayshot@gmail.com>
2024-03-13 15:58:30 +0100

    fix-webhook

--- handlers/webhookController.js
+++ handlers/webhookController.js
@@ -3,26 +3,27 @@ const { sendMessage } = require("../utils/sendMessage");
 const receiveWebhook = async (req, res) => {
   // const formData = req.body;
   // res.status(200).json({ formData });
-  const message = req.body.messages; //.messages.text
+  const message = req.body.message.text; //.messages.text
   console.log(message);
 
-  // const data = {
-  //   typing_time: 10,
-  //   to: req.body.messages.chat_id,
-  //   body: "Hello, world!",
-  // };
+  const body = {
+    typing_time: 10,
+    to: req.body.messages.chat_id,
+    body: "Hello, world!",
+  };
 
-  // if (message === "hello") {
-  //   sendMessage(data);
-  //   // await sendMessage("Hi there!");
-  // } else if (message === "help") {
-  //   const defData = {
-  //     typing_time: 10,
-  //     to: req.body.messages.chat_id,
-  //     body: "How can i help today!",
-  //   };
-  //   await sendMessage(defData);
-  // }
+  const defBody = {
+    typing_time: 10,
+    to: req.body.messages.chat_id,
+    body: "How can i help you today?",
+  };
+
+  if (message === "hello") {
+    sendMessage(body);
+    // await sendMessage("Hi there!");
+  } else {
+    await sendMessage(defBody);
+  }
 
   res.sendStatus(200);
 };
--- utils/sendMessage.js
+++ utils/sendMessage.js
@@ -1,9 +1,9 @@
 const { default: axios } = require("axios");
 
-const sendMessage = async (data) => {
+const sendMessage = async (body) => {
   const token = process.env.WHAPI;
   const url = `https://gate.whapi.cloud/messages/text?token=${token}`;
-  const response = await axios(url, data, {
+  const response = await axios(url, body, {
     headers: {
       "Content-Type": "application/json",
     },
